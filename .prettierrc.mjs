e2cf45b0a072d76f73165efc3a24de6437ed033b
Sergey Kupletsky <s.kupletsky@gmail.com>
2025-09-09 15:52:23 +0200

    chore: setup new code style rules

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import zavoloklomPrettierConfig from '@zavoloklom/prettier-config';
+
+const config = {
+  ...zavoloklomPrettierConfig,
+};
+
+// eslint-disable-next-line import/no-default-export
+export default config;
---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import config from '@zavoloklom/eslint-config';
+import { defineConfig } from 'eslint/config';
+import ava from 'eslint-plugin-ava';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig([
+  ...config,
+  // TSCONFIG
+  {
+    name: 'Typescript import/resolver',
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.eslint.json',
+        },
+      },
+    },
+  },
+  {
+    name: 'Typescript languageOptions',
+    languageOptions: {
+      parserOptions: {
+        projectService: false,
+        project: './tsconfig.eslint.json',
+      },
+    },
+  },
+  // For AVA tests
+  ava.configs['flat/recommended'],
+  {
+    ignores: ['.tsimp/*', 'coverage/*', 'dist/*'],
+  },
+]);
